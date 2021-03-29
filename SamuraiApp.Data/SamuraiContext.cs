4afe1b6c1703fe673e3caa2a4fff82b3b79e0162
unknown <reagan@barringtonfamily.net>
2021-03-29 14:17:47 -0400

    3 projects

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext:DbContext
+    {
+        // provides logic for EF Core to interact with the database
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
